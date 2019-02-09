bb059ec024ea050eb05d823c5d39d8ac119ef989
Ed Mamerto <edmamerto@gmail.com>
2019-02-09 12:58:18 -0500

    fix naming conventions and add docs

--- firstClassCitizens.js
+++ firstClassCitizens.js
@@ -1,3 +1,8 @@
+// It means that when functions can be treated like a variable, 
+// meaning they can be passed as arguments to other 
+// functions as well or can be assigned as a value 
+// to a variable or even return in a function
+
 const calc = () => {
 	return 4 * 3
 }
